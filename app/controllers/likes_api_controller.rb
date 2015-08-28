079c8e1ee2cc1e5f7763b80fe51608980127d374
Tylor Shin <shincode@icloud.com>
2015-08-28 13:12:23 +0900

    Fixed comment_like notification
    Fixed Bugs.

--- app/controllers/likes_api_controller.rb
+++ app/controllers/likes_api_controller.rb
@@ -8,7 +8,7 @@ class LikesApiController < ApplicationController
       if @like.save
         # Make notification to receiver
         unless Comment.find(params[:comments_api_id]).user === current_user
-          @like.comment_like_send_noti(params[:posts_api_id], params[:comments_api_id], current_user.id, Post.find(params[:posts_api_id]).user.id)
+          @like.comment_like_send_noti(params[:posts_api_id], params[:comments_api_id], current_user.id, Comment.find(params[:comments_api_id]).user.id)
         end
       end
       @comments = Comment.where(post_id: params[:posts_api_id])
@@ -22,7 +22,7 @@ class LikesApiController < ApplicationController
       if  @like.destroy
         # Destroy notification to receiver
         unless Comment.find(params[:comments_api_id]).user === current_user
-          @like.comment_like_undo_noti(params[:posts_api_id], params[:comments_api_id], current_user.id, Post.find(params[:posts_api_id]).user.id)
+          @like.comment_like_undo_noti(params[:posts_api_id], params[:comments_api_id], current_user.id, Comment.find(params[:comments_api_id]).user.id)
         end
       end
       @comments = Comment.where(post_id: params[:posts_api_id])
